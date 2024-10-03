7a6c339d7f247f95e993ce75278ee614d7e080f6
Mark Whitaker <mark@bitrot.net>
2024-10-03 07:02:19 +0100

    Simplify getGitVersion() in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,13 +17,11 @@ dependencies {
     testImplementation("junit:junit:$junitVersion")
 }
 
-fun getGitVersion(): String {
-    try {
-        val process = ProcessBuilder("git", "describe", "--tags", "--always").start()
-        val output = process.inputStream.bufferedReader().readLine()
-        process.waitFor()
-        return output
-    } catch (e: Exception) {
-        return "1.0.0"
-    }
+fun getGitVersion() = try {
+    val process = ProcessBuilder("git", "describe", "--tags", "--always").start()
+    val output = process.inputStream.bufferedReader().readLine()
+    process.waitFor()
+    output
+} catch (_: Exception) {
+    "1.0.0"
 }
